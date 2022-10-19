251876d3a1118a5bffb3ce0db5f3a6ac4a093dd6
CJCU-User <CJCU-User@L2B102-023>
2022-10-19 16:19:44 +0800

    加入專案檔案。

---
+++ Todoltems/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todoltems.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public String Firstname { get; set; }
+        [Required]
+        public String Lastname { get; set; }
+        [Required]
+        public String Email { get; set; }
+    }
+}
---
+++ Todoltems/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Todoltems.Dtos;
+using Todoltems.Models;
+
+namespace Todoltems.Repositories
+{
+    public interface IUserRepository 
+    {
+        // 查詢所有 Users 資料
+        public Task<IEnumerable<User>> GetAll();
+        // 查詢指定 id 的單一 User 資料
+        public Task<User> GetById(Guid id);
+        // 新增 User 資料
+        public Task Create(CreateUserDTO createUserDTO);
+    }
+}
